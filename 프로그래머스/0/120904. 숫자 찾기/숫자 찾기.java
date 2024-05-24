8d31c1d35520efa89de9cf6dc3a76234f6893122
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-05-24 15:04:43 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num, int k) {
+        
+        String str = String.valueOf(num);
+        
+        int answer = str.indexOf(String.valueOf(k));
+        
+        if (answer == -1) {
+        	return answer = -1;
+        } else {
+        	return answer + 1;
+        }
+    }
+}
