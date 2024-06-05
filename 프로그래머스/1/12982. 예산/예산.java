dc9d1bf1ab4f14c041d417a802a139e561aa7226
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-06-05 20:43:22 +0900

    [level 1] Title: 예산, Time: 0.49 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        Arrays.sort(d);
+        
+        for (int c : d) {
+        	sum += c;
+        	if (sum > budget) break;
+        	answer++;
+        }
+        
+        return answer;
+    }
+}
