7438752a51860431f4d68b6e080196d647e74361
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-06-07 19:00:31 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -3,23 +3,25 @@ class Solution {
 
 		int left = 0;
 		int right = 0;
-
+		int end = 0;
 		for (int i = 0; i < goal.length; i++) {
 			boolean flag = true;
 
-			if (cards1[left].equals(goal[i])) {
-				left += 1;
-				flag = false;
+			if (left < cards1.length) {
+				if (cards1[left].equals(goal[i])) {
+					left += 1;
+					flag = false;
+				}
 			}
-
-			if (cards2[right].equals(goal[i])) {
-				right += 1;
-				flag = false;
+			if (right < cards2.length) {
+				if (cards2[right].equals(goal[i])) {
+					right += 1;
+					flag = false;
+				}
 			}
 			if (flag)
 				return "No";
 		}
-
 		return "Yes";
 	}
 }
