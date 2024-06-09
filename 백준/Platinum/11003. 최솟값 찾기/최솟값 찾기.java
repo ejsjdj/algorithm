27f53c78c21c88b1e44735cb160fc150454d07ab
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-06-09 14:36:12 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2360 ms, Memory: 619504 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -12,35 +12,34 @@ public class Main {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(bf.readLine());
-		Deque<Node> mydeque = new LinkedList<>();
+		int len = Integer.parseInt(st.nextToken());
+		int scale = Integer.parseInt(st.nextToken());
+		Deque<Node> deque = new LinkedList<>();
 		
-		for (int i = 0; i < N; i++) {
+		st = new StringTokenizer(bf.readLine());
+		for (int i = 0 ; i < len; i++) {
 			int now = Integer.parseInt(st.nextToken());
-			
-			while (!mydeque.isEmpty() && mydeque.getLast().value > now) {
-				mydeque.removeLast();
+			while (!deque.isEmpty() && deque.getLast().value >= now) {
+				deque.removeLast();
 			}
-			mydeque.addLast(new Node(now,i));
-			
-			if (mydeque.getFirst().index <= i - L) {
-				mydeque.removeFirst();
+			deque.addLast(new Node(i, now));
+			if (deque.getFirst().idx <= i - scale) {
+				deque.removeFirst();
 			}
-			bw.write(mydeque.getFirst().value + " ");
+			bw.write(deque.getFirst().value + " ");
 		}
 		bw.flush();
 		bw.close();
 		
 	}
+	
 	static class Node {
-		public int value;
-		public int index;
+		int idx;
+		long value;
 		
-		Node(int value, int index) {
+		Node(int idx, int value) {
+			this.idx = idx;
 			this.value = value;
-			this.index = index;
 		}
 	}
 }
