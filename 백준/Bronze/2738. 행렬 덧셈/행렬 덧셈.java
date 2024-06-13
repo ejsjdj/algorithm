94d844d10c49db16bf76c26afa01d8d99c3b097e
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-06-13 19:48:46 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 240 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int matrix1[][] = new int[N][M];
+		int matrix2[][] = new int[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < M; j++) {
+				matrix1[i][j] = Integer.parseInt(st.nextToken()); 
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < M; j++) {
+				matrix2[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				bw.write(matrix1[i][j] + matrix2[i][j] + " ");
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+		bf.close();
+	}
+}
