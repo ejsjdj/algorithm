b7ee71865668d711e07040dd9bac313d235ba3c6
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-06-14 10:40:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.48 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+    	
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        String answer = "";
+        
+        for (int i = 0; i < participant.length; i++) {
+        	if (hm.containsKey(participant[i])) hm.replace(participant[i],hm.get(participant[i])+1);
+        	else hm.put(participant[i], 1);
+        }
+        
+        for (int i = 0; i < completion.length; i++) {
+        	hm.replace(completion[i], hm.get(completion[i])-1);
+        }
+        
+        for (int i = 0; i < participant.length; i++) {
+        	if (hm.get(participant[i]) == 1) {
+        		answer = participant[i];
+        		break;
+        	}
+        }
+        
+        return answer;
+    }
+}
