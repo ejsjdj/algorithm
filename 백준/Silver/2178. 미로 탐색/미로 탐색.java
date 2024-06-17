64c0404047e068186050fd5c6b05632617992e3e
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-06-18 00:25:26 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,45 +6,51 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dx = { 0, 1, 0, -1 };
-	static int[] dy = { 1, 0, -1, 0 };
-	static boolean[][] visited;
 	static int[][] A;
-	static int N, M;
+	static int[][] answer;
+	static boolean[][] visited;
+	static int[] combination_x = { -1, 0, 1, 0 };
+	static int[] combination_y = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		A = new int[N][M];
-		visited = new boolean[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			String line = st.nextToken();
-			for (int j = 0; j < M; j++) {
-				A[i][j] = Integer.parseInt(line.substring(j, j + 1));
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+
+		A = new int[M][N];
+		for (int i = 0; i < M; i++) {
+			String str = bf.readLine();
+			for (int j = 0; j < N; j++) {
+				A[i][j] = str.charAt(j) - '0';
 			}
 		}
-		BFS(0, 0);
-		System.out.println(A[N - 1][M - 1]);
+
+		answer = new int[M][N];
+		answer[0][0] = 1;
+		visited = new boolean[M][N];
+
+		BFS(M, N);
+		System.out.println(answer[M - 1][N - 1]);
 	}
 
-	public static void BFS(int i, int j) {
+	private static void BFS(int M, int N) {
 		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] { i, j });
+		queue.add(new int[] { 0, 0 });
+		visited[0][0] = true;
 		while (!queue.isEmpty()) {
-			int now[] = queue.poll();
-			visited[i][j] = true;
-			for (int k = 0; k < 4; k++) {
-				int x = now[0] + dx[k];
-				int y = now[1] + dy[k];
-				if (x >= 0 && y >= 0 && x < N && y < M) { // 좌표 유효성 검사
-					if (A[x][y] != 0 && !visited[x][y]) {
-						visited[x][y] = true;
-						A[x][y] = A[now[0]][now[1]] + 1;
-						queue.add(new int[] { x, y });
-					}
+			int[] L = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int x = L[0];
+				x += combination_x[i];
+				int y = L[1];
+				y += combination_y[i];
+				if (0 <= x && x < M && 0 <= y && y < N && !visited[x][y] && A[x][y] == 1) {
+					visited[x][y] = true;
+					answer[x][y] = answer[L[0]][L[1]] + 1;
+					queue.add(new int[] { x, y });
 				}
 			}
 		}
