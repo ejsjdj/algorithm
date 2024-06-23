6c60c5560829f284eba90379b196541d1b9a6b7d
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-06-23 17:05:07 +0900

    [Gold I] Title: K번째 수, Time: 248 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		long start  = 1;
+		long end = K;
+		long ans = 0;
+		
+
+		while (start <= end) {
+			long middle = (start + end) / 2;
+			long cnt = 0;
+			
+			for (int i = 1; i <= N; i++) {
+				cnt += Math.min(middle / i, N);
+			}
+			
+			if (cnt < K) {
+				start = middle + 1;
+			} else {
+				ans = middle;
+				end = middle - 1;
+			}
+		}
+		
+		System.out.println(ans);
+		
+		sc.close();
+	}
+}
