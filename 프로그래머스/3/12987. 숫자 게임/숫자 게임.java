1106abf832fb49c0e3fd95ae4fcbc5d4cf59df17
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-07-25 18:16:07 +0900

    [level 3] Title: 숫자 게임, Time: 67.13 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int a = 0;
+        int b = 0;
+        
+        while (a < A.length && b < B.length) {
+            if (B[b] > A[a]) {
+                answer++; // B의 요소가 A의 요소보다 클 때 점수 증가
+                a++; // A의 다음 요소로 이동
+            }
+            b++; // B의 다음 요소로 이동
+        }
+        
+        return answer;
+    }
+}
