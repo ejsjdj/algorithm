d29ccdc85955747af7c4780ee0d036528a995e33
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-07-31 21:29:16 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String m, String[] musicinfos) {
+        String answer = "(None)";
+        int maxTime = 0;
+
+        m = m.replace("C#", "H").replace("D#", "I").replace("F#", "J").replace("G#", "K").replace("A#", "L").replace("B#", "K");
+
+        for (String info : musicinfos) {
+            String[] now = info.split(",");
+            int start = Integer.parseInt(now[0].split(":")[0]) * 60 + Integer.parseInt(now[0].split(":")[1]);
+            int end = Integer.parseInt(now[1].split(":")[0]) * 60 + Integer.parseInt(now[1].split(":")[1]);
+            int time = (end >= start) ? (end - start) : (end + 24 * 60 - start);
+
+            String title = now[2];
+            String score = now[3].replace("C#", "H").replace("D#", "I").replace("F#", "J").replace("G#", "K").replace("A#", "L").replace("B#","K");
+            String nowMusic = score.repeat(time / score.length()) + score.substring(0, time % score.length());
+
+            if (nowMusic.contains(m)) {
+                if (time > maxTime) {
+                    maxTime = time;
+                    answer = title;
+                }
+            }
+        }
+        return answer;
+    }
+}
