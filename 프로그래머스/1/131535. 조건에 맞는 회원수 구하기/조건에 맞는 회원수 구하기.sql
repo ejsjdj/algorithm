17a839b40e1e2d170b2e894ca49dea87a6f44be8
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-08-01 16:24:12 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT 
+    COUNT(*)
+        FROM USER_INFO
+        WHERE 20 <= AGE AND AGE <= 29 
+        AND JOINED BETWEEN TO_DATE('2021-01-01', 'YYYY-MM-DD') AND TO_DATE('2021-12-31', 'YYYY-MM-DD')
