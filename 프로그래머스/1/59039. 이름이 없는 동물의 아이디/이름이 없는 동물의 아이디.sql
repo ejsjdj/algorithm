2ff8a067730654bc937a202144d8495661a8734b
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-08-01 15:34:44 +0900

    [level 1] Title: 이름이 없는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
+++ 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
@@ -1,2 +1,6 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID  from ANIMAL_INS where NAME is NULL order by ANIMAL_ID asc;
+SELECT 
+    ANIMAL_ID
+        FROM ANIMAL_INS
+            WHERE NAME IS NULL
+                ORDER BY ANIMAL_ID;
