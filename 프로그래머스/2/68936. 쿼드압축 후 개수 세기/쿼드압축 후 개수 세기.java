aae57219732a8d73a29f56ed65613685c595e4eb
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-08-14 00:10:28 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 12.57 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,30 +1,38 @@
 class Solution {
-    int[] cnt = {0, 0};
-
-    void check(int[][] arr, int start_x, int start_y, int end_x, int end_y) {
-        int now = arr[start_x][start_y];
-
-
-        for (int i = start_x; i < end_x; i++) {
-            for (int j = start_y; j < end_y; j++) {
-                if (arr[i][j] != now) {
-
-                    int mid_x = (start_x + end_x) / 2;
-                    int mid_y = (start_y + end_y) / 2;
-
-                    check(arr, start_x, start_y, mid_x, mid_y);
-                    check(arr, mid_x, start_y, end_x, mid_y);
-                    check(arr, start_x, mid_y, mid_x, end_y);
-                    check(arr, mid_x, mid_y, end_x, end_y);
-                    return;
-                }
-            }
-        }
-        cnt[now]++;
-    }
-
-    public int[] solution(int[][] arr) {
-        check(arr, 0, 0, arr.length, arr.length);
+	
+	int[] cnt = {0, 0};
+	
+	void check(int[][] arr, int x, int y, int end_x, int end_y) {
+		
+		int now = arr[x][y];
+		
+		int mid_x = (x + end_x) / 2;
+		int mid_y = (y + end_y) / 2;
+		
+		for (int i = x; i < end_x; i++) {
+			
+			for (int j = y; j < end_y; j++) {
+				
+				if (arr[i][j] != now) {
+					
+					check(arr, x, y, mid_x, mid_y);
+					check(arr, mid_x, y, end_x, mid_y);
+					check(arr, x, mid_y, mid_x, end_y);
+					check(arr, mid_x, mid_y, end_x, end_y);
+					return ;
+				}
+			}
+		}
+		
+		cnt[now]++;
+		
+	}
+	
+	public int[] solution(int[][] arr) {
+        
+		int len = arr.length;
+		check(arr, 0, 0, len, len);
+        
         return cnt;
     }
 }
