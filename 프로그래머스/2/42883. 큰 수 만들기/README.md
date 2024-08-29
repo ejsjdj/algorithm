36d01041fbd8feaa9bcf7e75ad0a4eca4b602c6b
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-08-30 01:24:20 +0900

    [level 2] Title: 큰 수 만들기, Time: 62.32 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883?language=java) 
 
 ### 성능 요약
 
-메모리: 88.3 MB, 시간: 1843.00 ms
+메모리: 98.3 MB, 시간: 62.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 30일 01:07:06
+2024년 08월 30일 01:24:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,26 +1,19 @@
+import java.util.Stack;
 class Solution {
     public String solution(String number, int k) {
-    	
-    	StringBuilder sb = new StringBuilder();
-    	
-    	sb.append(number);
-    	int cnt = 0;
-    	
-    	for (int i = 0; i < sb.length()-1; i++) {
-    		if (i < 0) i = 0;
-    		if (sb.charAt(i) < sb.charAt(i+1)) {
-    			sb.deleteCharAt(i);
-    			cnt++;
-    			i -= 2;
-    			if (cnt == k) break;
-    		}
-    	}
-    	
-    	while (cnt != k) {
-    		sb.deleteCharAt(sb.length() - 1);
-    		cnt++;
-    	}
-    	
-    	return sb.toString();
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
     }
 }
