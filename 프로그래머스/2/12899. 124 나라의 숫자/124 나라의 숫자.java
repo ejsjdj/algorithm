3b5adf37a772212534261a39fe8dfc25b4c9ccc6
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-09-26 01:33:34 +0900

    [level 2] Title: 124 나라의 숫자, Time: 2.36 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,18 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        String[] numbers = {"4", "1", "2"}; // 나눳을때 값이 1이면 1, 2이면 2, 0이면 4
-        
-        while (n > 0) {
-        	int remainder = n % 3;
-        	n /= 3;
-        	
-        	if (remainder == 0) n--; // 나머지가 0이라면 3의 배수이므로 n에서 1을빼줌
-        	
-        	answer = numbers[remainder] + answer;
+    	String result = "";
+    	
+    	List<Integer> list = new ArrayList<>();
+        while (n != 0) {
+        	list.add(n % 3 == 0 ? 1 : n % 3);
+        	n = n / 3;
         }
         
+        for (int i = list.size() - 1; i >= 0; i--) {
+        	result += list.get(i);
+        }
         
-        return answer;
+        return result;
     }
 }
--- 프로그래머스/2/12899. 124 나라의 숫자/README.md
+++ 프로그래머스/2/12899. 124 나라의 숫자/README.md
@@ -1,6 +1,6 @@
 # [level 2] 124 나라의 숫자 - 12899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 01:33:14
+2024년 09월 26일 01:33:31
 
 ### 문제 설명
 
