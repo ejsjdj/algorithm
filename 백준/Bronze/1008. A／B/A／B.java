171d769b165f4044d173ab05545fc6ad696c853a
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-11-29 23:50:57 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner1 = new Scanner(System.in);
+		double x = scanner1.nextInt();
+		double y = scanner1.nextInt();
+		System.out.println(x / y);
+	}
+}
