f148d837f7a19a3ff32e9370fc01c1640e1239c1
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-11-29 23:49:09 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner1 = new Scanner(System.in);
+		int x = scanner1.nextInt();
+		int y = scanner1.nextInt();
+		System.out.println(x + y);
+	}
+}
