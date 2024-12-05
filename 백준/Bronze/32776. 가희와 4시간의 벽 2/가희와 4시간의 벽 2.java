e5409bc800e982ff4b78c6bd17a95f725f66e9e7
ejsjdj <63234082+ejsjdj@users.noreply.github.com>
2024-12-05 19:42:28 +0900

    [Bronze IV] Title: 가희와 4시간의 벽 2, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/32776. 가희와 4시간의 벽 2/가희와 4시간의 벽 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int S = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(bf.readLine());
+		int ma = Integer.parseInt(st.nextToken());
+		int F = Integer.parseInt(st.nextToken());
+		int mb = Integer.parseInt(st.nextToken());
+		
+		if (S <= 240) System.out.println("high speed rail");
+		else if (S <= F + ma + mb) System.out.println("high speed rail");
+		else System.out.println("flight");
+		
+		bf.close();
+	}
+}
