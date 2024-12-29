b81e5d339d4f946ff107f54e87654c71f66bc152
김기범 <63234082+ejsjdj@users.noreply.github.com>
2024-12-30 02:45:19 +0900

    [Silver II] Title: 병사 배치하기, Time: 136 ms, Memory: 14640 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = N-1; i >= 0; i--) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(N - lis(arr));
+		
+		br.close();
+	}
+	
+	static int lis(int[] arr) {
+		
+		List<Integer> lis = new ArrayList<>();
+		
+		for (int n : arr) {
+			int idx = Collections.binarySearch(lis, n);
+			if (idx < 0) idx = -(idx + 1);
+			if (idx == lis.size()) lis.add(n);
+			else lis.set(idx, n);
+		}
+		
+		return lis.size();
+	}
+}
