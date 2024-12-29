59a24e7703e95d212680e6d8e7f7a47d69c91682
김기범 <63234082+ejsjdj@users.noreply.github.com>
2024-12-30 02:46:50 +0900

    [Silver II] Title: 병사 배치하기, Time: 128 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -15,7 +15,7 @@ public class Main {
 		int[] arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		for (int i = N-1; i >= 0; i--) {
+		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
@@ -29,7 +29,7 @@ public class Main {
 		List<Integer> lis = new ArrayList<>();
 		
 		for (int n : arr) {
-			int idx = Collections.binarySearch(lis, n);
+			int idx = Collections.binarySearch(lis, n, Collections.reverseOrder());
 			if (idx < 0) idx = -(idx + 1);
 			if (idx == lis.size()) lis.add(n);
 			else lis.set(idx, n);
