fea78fd06c5294a218516a0850f5a719e9ae560a
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-01-13 01:16:18 +0900

    [Bronze II] Title: Euclid, Time: 104 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/9884. Euclid/Euclid.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		while (a != b) {
+			int A = Math.max(a, b) - Math.min(a, b);
+			int B = Math.min(a, b);
+			a = A;
+			b = B;
+		}
+		System.out.println(b);
+	}
+}
