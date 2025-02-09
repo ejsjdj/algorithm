2178623e6b3a37b632422e1a9d68f5e9e21f8a2d
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-02-10 06:30:32 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 43148 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,51 +1,59 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
-	
-	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	static int N, M;
-	static int distance[][];
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		N = Integer.parseInt(br.readLine());	// 도시의 수
-		M = Integer.parseInt(br.readLine());	// 버스의 수
-		distance = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (i == j) 
-					distance[i][j] = 0;
-				else distance[i][j] = 10000001;
-			}
-		}
-		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			if (distance[s][e] > v) distance[s][e] = v;
-		}
-		for (int k = 1; k <= N; k++) {
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					if (distance[i][j] > distance[i][k] + distance[k][j])
-						distance[i][j] = distance[i][k] + distance[k][j];
-				}
-			}
-		}
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (distance[i][j] == 10000001) System.out.print("0 ");
-				else System.out.print(distance[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 입력: 도시와 버스의 개수
+        int cityCount = Integer.parseInt(br.readLine());
+        int busCount = Integer.parseInt(br.readLine());
+
+        // 거리 배열 초기화
+        int[][] distance = new int[cityCount + 1][cityCount + 1];
+        final int INF = Integer.MAX_VALUE / 2; // 무한대 값 (오버플로우 방지)
+
+        for (int i = 1; i <= cityCount; i++) {
+            for (int j = 1; j <= cityCount; j++) {
+                if (i == j) distance[i][j] = 0; // 자기 자신으로 가는 경로는 0
+                else distance[i][j] = INF;     // 초기값은 무한대
+            }
+        }
+
+        // 입력: 버스 정보 처리
+        for (int i = 0; i < busCount; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int startCity = Integer.parseInt(st.nextToken());
+            int endCity = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            // 최소 비용만 저장
+            distance[startCity][endCity] = Math.min(distance[startCity][endCity], cost);
+        }
+
+        // 플로이드-워셜 알고리즘: 모든 경로에 대한 최소 비용 계산
+        for (int via = 1; via <= cityCount; via++) {       // 경유 도시
+            for (int from = 1; from <= cityCount; from++) { // 출발 도시
+                for (int to = 1; to <= cityCount; to++) {   // 도착 도시
+                    if (distance[from][to] > distance[from][via] + distance[via][to]) {
+                        distance[from][to] = distance[from][via] + distance[via][to];
+                    }
+                }
+            }
+        }
+
+        // 결과 출력: 연결되지 않은 경우 0 출력
+        for (int i = 1; i <= cityCount; i++) {
+            for (int j = 1; j <= cityCount; j++) {
+                if (distance[i][j] == INF) sb.append(0).append(" ");
+                else sb.append(distance[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
