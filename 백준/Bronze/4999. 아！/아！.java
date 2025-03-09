c63e0a08de6ffaa020ba34459dd964675943ab93
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-03-09 21:05:32 +0900

    [Bronze V] Title: 아!, Time: 172 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str1 = sc.next();
+		String str2 = sc.next();
+		
+		if (str1.length() >= str2.length()) System.out.println("go");
+		else System.out.println("no");
+		
+	}
+}
