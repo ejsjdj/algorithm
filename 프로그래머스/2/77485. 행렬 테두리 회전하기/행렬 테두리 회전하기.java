4dd979cc726443afc0e3a9603bba1ee30ba6d828
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-04-21 03:20:30 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 11.82 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,61 +1,48 @@
 class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
-    	
-        int[] answer = new int[queries.length];
+        int[][] matrix = new int[rows][columns];
+        int[] result = new int[queries.length];
         
-        int[][] arr = new int[rows][columns];
-        int num = 1;
-        
-        
-        
-        for (int i = 0; i < rows; i++) {
-        	for (int j = 0; j < columns; j++) {
-        		arr[i][j] = num++;
-        	}
+        for (int i = 0, num = 1; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = num++;
+            }
         }
         
         for (int i = 0; i < queries.length; i++) {
-        	answer[i] = move(arr, queries[i]);
+            result[i] = rotate(matrix, queries[i]);
         }
         
-        return answer;
+        
+        return result;
     }
-    
-    int move(int[][] arr, int[] queries) {
-    	int min = Integer.MAX_VALUE;
-    	int row1 = queries[0] - 1;	// 1
-    	int col1 = queries[1] - 1;	// 1
-    	int row2 = queries[2] - 1;	// 2
-    	int col2 = queries[3] - 1;	// 2
-    	
-    	int next = 0;
-    	int last = arr[row1+1][col1];
-    	
-    	for (int i = col1; i < col2; i++) {
-    		min = Math.min(min, arr[row1][i]);
-    		next = arr[row1][i];
-    		arr[row1][i] = last;
-    		last = next;
-    	}
-    	for (int i = row1; i < row2; i++) {
-    		min = Math.min(min, arr[i][col2]);
-    		next = arr[i][col2];
-    		arr[i][col2] = last;
-    		last = next;
-    	}
-    	for (int i = col2; i > col1; i--) {
-    		min = Math.min(min, arr[row2][i]);
-    		next = arr[row2][i];
-    		arr[row2][i] = last;
-    		last = next;
-    	}
-    	for (int i = row2; i > row1; i--) {
-    		min = Math.min(min, arr[i][col1]);
-    		next = arr[i][col1];
-    		arr[i][col1] = last;
-    		last = next;
-    	}
-    	
-    	return min;
+
+    private int rotate(int[][] matrix, int[] query) {
+        int row1 = query[0] - 1;
+        int col1 = query[1] - 1;
+        int row2 = query[2] - 1;
+        int col2 = query[3] - 1;
+        
+        int temp = matrix[row1][col1];
+        int min = temp;
+        
+        for (int i = row1; i < row2; i++) {
+        	matrix[i][col1] = matrix[i+1][col1];
+        	min = Math.min(min, matrix[i][col1]);
+        }
+        for (int i = col1; i < col2; i++) {
+        	matrix[row2][i] = matrix[row2][i+1];
+        	min = Math.min(min, matrix[row2][i]);
+        }
+        for (int i = row2; i > row1; i--) {
+        	matrix[i][col2] = matrix[i-1][col2];
+        	min = Math.min(min, matrix[i][col2]);
+        }
+        for (int i = col2; i > col1 + 1; i--) {
+        	matrix[row1][i] = matrix[row1][i-1];
+        	min = Math.min(min, matrix[row1][i]);
+        }
+        matrix[row1][col1+1] = temp;
+        return min;
     }
 }
