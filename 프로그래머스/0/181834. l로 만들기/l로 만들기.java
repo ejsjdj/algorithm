1fc7b2d7569d99b4b1c0bb1dfce18e0e055df6b5
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-05-09 21:02:40 +0900

    [level 0] Title: l로 만들기, Time: 1.58 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        myString = myString.replace("a", "l");
+        myString = myString.replace("b", "l");
+        myString = myString.replace("c", "l");
+        myString = myString.replace("d", "l");
+        myString = myString.replace("e", "l");
+        myString = myString.replace("f", "l");
+        myString = myString.replace("g", "l");
+        myString = myString.replace("h", "l");
+        myString = myString.replace("i", "l");
+        myString = myString.replace("j", "l");
+        myString = myString.replace("k", "l");        
+        answer = myString;
+        return answer;
+    }
+}
