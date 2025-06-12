fbd71394be0a3bd1e144b28608e5a29f4e3a9930
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-06-12 21:21:30 +0900

    [Gold III] Title: ACM Craft, Time: 752 ms, Memory: 243384 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,68 +1,62 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt(); // 테스트 케이스 개수
-
-        while (T-- > 0) {
-            int N = sc.nextInt(); // 건물 개수
-            int K = sc.nextInt(); // 건설 순서 규칙 개수
-
-            int[] buildTime = new int[N + 1]; // 각 건물의 건설 시간
-            for (int i = 1; i <= N; i++) {
-                buildTime[i] = sc.nextInt();
-            }
-
-            // 그래프와 진입차수 배열
-            ArrayList<Integer>[] graph = new ArrayList[N + 1];
-            for (int i = 1; i <= N; i++) {
-                graph[i] = new ArrayList<>();
-            }
-            int[] indegree = new int[N + 1];
-
-            // 건설 규칙 입력
-            for (int i = 0; i < K; i++) {
-                int X = sc.nextInt();
-                int Y = sc.nextInt();
-                graph[X].add(Y);
-                indegree[Y]++;
-            }
-
-            int W = sc.nextInt(); // 목표 건물
-
-            // 위상정렬 준비
-            Queue<Integer> queue = new LinkedList<>();
-            int[] result = new int[N + 1]; // 각 건물의 최소 완성 시간
-
-            // 진입차수 0인 노드(선행 건물 없는 건물) 큐에 넣기
-            for (int i = 1; i <= N; i++) {
-                result[i] = buildTime[i];
-                if (indegree[i] == 0) {
-                    queue.offer(i);
-                }
-            }
-
-            // 위상정렬 수행
-            while (!queue.isEmpty()) {
-                int now = queue.poll();
-
-                for (int next : graph[now]) {
-                    // 다음 건물의 최소 건설 시간 갱신
-                    if (result[next] < result[now] + buildTime[next]) {
-                        result[next] = result[now] + buildTime[next];
-                    }
-                    indegree[next]--;
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
-                    }
-                }
-            }
-
-            // 결과 출력
-            System.out.println(result[W]);
-        }
-        sc.close();
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	int T = Integer.parseInt(st.nextToken());
+    	
+    	while (T-- > 0) {
+    		st = new StringTokenizer(br.readLine());
+    		int N = Integer.parseInt(st.nextToken());	// 건물의 개수
+    		int K = Integer.parseInt(st.nextToken());	// 건설 규칙의 개수
+    		
+    		int[] buildTime = new int[N + 1];	// 건설 시간을 저장할 배열
+    		int[] buildTime2 = new int[N + 1];	// 건설 시간을 저장할 배열
+    		int[] buildRequisition = new int[N + 1];	// 건설 필요 조건의 수를 저장할 배열
+    		List<List<Integer>> buildOrder = new ArrayList<>();	// 해당 건물이 건설됬을때 건설가능해지는 건물을 저장하는 리스트
+    		for (int i = 0; i < N + 1; i++) {
+    			buildOrder.add(new ArrayList<>());
+    		}
+    		st = new StringTokenizer(br.readLine());
+    		for (int i = 1; i < N + 1; i++) {
+    			int time = Integer.parseInt(st.nextToken());	// 각 건물의 건설 시간을 저장
+    			buildTime[i] = time;
+    			buildTime2[i] = time;
+    		}
+    		
+    		for (int i = 0; i < K; i++) {	// 건설 규칙을 입력
+    			st = new StringTokenizer(br.readLine());
+    			int require = Integer.parseInt(st.nextToken());
+    			int complement = Integer.parseInt(st.nextToken());
+    			buildRequisition[complement]++;
+    			buildOrder.get(require).add(complement);
+    		}
+    		
+    		Queue<Integer> queue = new LinkedList<>();
+    		for (int i = 1; i < N + 1; i++) {
+    			if (buildRequisition[i] == 0) queue.add(i);
+    		}
+    		
+    		while (!queue.isEmpty()) {
+    			int now = queue.poll();
+    			for (int i : buildOrder.get(now)) {
+    				if (buildTime[i] + buildTime2[now] > buildTime2[i]) buildTime2[i] = buildTime[i] + buildTime2[now];
+    				buildRequisition[i]--;
+    				if (buildRequisition[i] == 0) queue.add(i);
+    			}
+    		}
+    		
+    		st = new StringTokenizer(br.readLine());
+    		System.out.println(buildTime2[Integer.parseInt(st.nextToken())]);
+    	}
     }
 }
