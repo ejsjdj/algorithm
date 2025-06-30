d4797a9d019d791b02fb5c35491d9be3acc3d048
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-06-30 20:48:10 +0900

    [Silver IV] Title: 다음 소수, Time: 392 ms, Memory: 14856 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            long num = Long.parseLong(br.readLine());
+            while (true) {
+                if (isPrime(num)) {
+                    System.out.println(num);
+                    break;
+                }
+                num++;
+            }
+        }
+    }
+
+    static boolean isPrime(long n) {
+        if (n < 2) return false;
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
