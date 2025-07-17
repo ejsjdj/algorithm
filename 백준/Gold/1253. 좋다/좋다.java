b5ae282be98582ef651afaaf53fd91e0f4aaadf5
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-07-18 07:51:57 +0900

    [Gold IV] Title: 좋다, Time: 164 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -5,42 +5,43 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		int len = Integer.parseInt(st.nextToken());
-		long[] arr = new long[len];
-		int answer = 0;
-		
-		st = new StringTokenizer(bf.readLine());
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Long.parseLong(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		for (int i = 0; i < arr.length; i++) {
-			long k = arr[i];
-			int left = 0;
-			int right = len - 1;
-			while (left < right) {
-				if (k == arr[left] + arr[right]) {
-					if (left != i && right != i) {
-						answer++;
-						break;
-					} else if (left == i) {
-						left++;
-					} else if (right == i) {
-						right--;
-					}
-				} else if (arr[left] + arr[right] < k) {
-					left++;
-				} else if (arr[left] + arr[right] > k) {
-					right--;
-				}
-			}
-		}
-		System.out.println(answer);
-		bf.close();
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int result = 0;
+        long A[] = new long[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        for (int i = 0; i < N; i++) {
+            int start = 0;
+            int end = N - 1;
+
+            while (start < end) {
+                if (A[start] + A[end] == A[i]) {
+                    if (start != i && end != i) {
+                        result++;
+                        break;
+                    } else if (start == i) {
+                        start++;
+                    } else if (end == i) {
+                        end--;
+                    }
+                }
+                else if (A[start] + A[end] < A[i]) {
+                    start = start + 1;
+                } else if (A[start] + A[end] > A[i]) {
+                    end = end - 1;
+                }
+            }
+        }
+        System.out.println(result);
+    }
 }
