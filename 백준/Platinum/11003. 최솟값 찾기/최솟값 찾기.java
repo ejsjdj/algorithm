e486496d20dbc15d83205e26115c3819f607741d
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-07-18 10:46:39 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2136 ms, Memory: 535828 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,45 +1,50 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+class Node {
+    int value;
+    int index;
+
+    public Node(int value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+}
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int len = Integer.parseInt(st.nextToken());
-		int scale = Integer.parseInt(st.nextToken());
-		Deque<Node> deque = new LinkedList<>();
-		
-		st = new StringTokenizer(bf.readLine());
-		for (int i = 0 ; i < len; i++) {
-			int now = Integer.parseInt(st.nextToken());
-			while (!deque.isEmpty() && deque.getLast().value >= now) {
-				deque.removeLast();
-			}
-			deque.addLast(new Node(i, now));
-			if (deque.getFirst().idx <= i - scale) {
-				deque.removeFirst();
-			}
-			bw.write(deque.getFirst().value + " ");
-		}
-		bw.flush();
-		bw.close();
-		
-	}
-	
-	static class Node {
-		int idx;
-		long value;
-		
-		Node(int idx, int value) {
-			this.idx = idx;
-			this.value = value;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        Deque<Node> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            int index = i;
+
+            while  (!deque.isEmpty() && deque.getLast().value > value) {
+                deque.removeLast();
+            }
+
+            deque.addLast(new Node(value, index));
+
+            while (!deque.isEmpty() && deque.getFirst().index < index - L + 1) {
+                deque.removeFirst();
+            }
+
+            bw.write(deque.getFirst().value + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
