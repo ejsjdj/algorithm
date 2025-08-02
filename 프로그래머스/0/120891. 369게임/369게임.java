cdc7134c658bcc3dbb682c7d88013e87ff9a9289
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-08-02 23:06:50 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        
         while (order > 0) {
         	if (order % 10 == 3) answer += 1;
         	else if (order % 10 == 6) answer += 1;
         	else if (order % 10 == 9) answer += 1;
         	order /= 10;
         }
-        
         return answer;
     }
 }
