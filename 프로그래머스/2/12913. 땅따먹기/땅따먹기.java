4398497a256ec3d0e8567ccde1f63e9af795e89a
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-08-03 18:09:48 +0900

    [level 2] Title: 땅따먹기, Time: 12.90 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,10 +1,9 @@
 class Solution {	
     int solution(int[][] land) {
-    	int result = 0;
+    	int answer = 0;
     	
     	for (int i = 1; i < land.length; i++) {
     		for (int j = 0; j < 4; j++) {
-    			
     			int max = 0;
     			
     			for (int k = 0; k < 4; k++) {
@@ -16,8 +15,8 @@ class Solution {
     		}
     	}
     	for (int i = 0; i < 4; i++) {
-    		if (result < land[land.length - 1][i]) result = land[land.length - 1][i];
+    		if (answer < land[land.length - 1][i]) answer = land[land.length - 1][i];
     	}
-    	return result;
+    	return answer;
     }
 }
