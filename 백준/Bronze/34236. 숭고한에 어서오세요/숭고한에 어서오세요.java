219b5ebf6963a3cfed47f02f57b7debca97487f3
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-09-03 19:38:39 +0900

    [Bronze III] Title: 숭고한에 어서오세요, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/34236. 숭고한에 어서오세요/숭고한에 어서오세요.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int diff = arr[1] - arr[0];
+		
+		System.out.println(arr[arr.length - 1] + diff);
+		
+	}
+}
