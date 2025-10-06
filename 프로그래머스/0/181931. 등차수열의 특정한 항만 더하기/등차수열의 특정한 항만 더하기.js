1a8635d6e869affd07f887b41f9f51408df55b27
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-10-06 23:46:58 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,7 @@
+function solution(a, d, included) {
+            var answer = 0;
+            for (i = 0; i < included.length; i++) {
+                if (included[i]) answer += a + (d * i)
+            }
+            return answer;
+        }
