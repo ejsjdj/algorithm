bba78a73e759b1113a61c159af9ca3d3815af24b
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-10-10 00:12:17 +0900

    [Gold IV] Title: 행렬 제곱, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
