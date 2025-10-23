8f8ff730d3df73c3a3d267ca86d81d9597e3d8b7
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-10-23 12:09:21 +0900

    [level 3] Title: 거스름돈, Time: 19.38 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        
+        for (int coin : money) {
+        		for (int i = coin; i < n + 1; i++) {
+        			dp[i] += dp[i - coin];
+        		}
+        }
+        
+        return dp[n];
+    }
+}
