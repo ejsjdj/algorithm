f7041baa91105f606e6db2ad380c6f357ee1a28c
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-11-19 20:51:33 +0900

    [Silver I] Title: 엔비스카의 영혼, Time: 128 ms, Memory: 18144 KB -BaekjoonHub

--- 백준/Silver/16568. 엔비스카의 영혼/엔비스카의 영혼.java
+++ 백준/Silver/16568. 엔비스카의 영혼/엔비스카의 영혼.java
@@ -1,32 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-
-        int[] dp = new int[N + 1];
-        dp[N] = 0;
-        
-        for (int i = N - 1; i >= 0; i--) {
-            dp[i] = dp[i + 1] + 1; // 기다리기
-            
-            if (i + a + 1 <= N) {
-                dp[i] = Math.min(dp[i], dp[i + a + 1] + 1);
-            }
-            
-            if (i + b + 1 <= N) {
-                dp[i] = Math.min(dp[i], dp[i + b + 1] + 1);
-            }
-        }
-        
-        System.out.println(dp[0]);
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N + 1];
+		Arrays.fill(arr, Integer.MAX_VALUE);
+		
+		arr[0] = 0;
+		
+		// 새치기 하는것 구현
+		for (int i = 0; i <= N; i++) {
+			if (i + 1 <= N) arr[i + 1] = Math.min(arr[i + 1], arr[i] + 1);
+			if (i + a + 1 <= N) arr[i + a + 1] = Math.min(arr[i] + 1, arr[i + a + 1]);
+			if (i + b + 1 <= N) arr[i + b + 1] = Math.min(arr[i] + 1, arr[i + b + 1]);
+		}
+		
+		System.out.println(arr[N]);
+	}
 }
