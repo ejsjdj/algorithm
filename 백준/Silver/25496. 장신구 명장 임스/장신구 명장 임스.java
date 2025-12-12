ff03af73760999b6fad74253c65783dc6b171ded
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-12-12 23:09:15 +0900

    [Silver V] Title: 장신구 명장 임스, Time: 112 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/25496. 장신구 명장 임스/장신구 명장 임스.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int P = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int cnt = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(P >= 200) break;
+            P += arr[i];
+            cnt++;
+        }
+        System.out.println(cnt);
+        br.close();
+    }
+}
