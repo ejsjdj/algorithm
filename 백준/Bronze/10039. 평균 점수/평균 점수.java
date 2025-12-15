070ec88aa09c11d8bba6c42125570225365eb190
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-12-15 20:42:13 +0900

    [Bronze IV] Title: 평균 점수, Time: 180 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int sum = 0;
+        
+        for (int i = 0; i < 5; i++) {
+            int score = sc.nextInt();
+            
+            if (score < 40) {
+                sum += 40;
+            } else {
+                sum += score;
+            }
+        }
+        
+        System.out.println(sum / 5);
+        
+        sc.close();
+    }
+}
