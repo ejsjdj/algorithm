b1413a8dfca96cf9cce4baf8d5157a644c0cd8fa
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-12-17 00:14:03 +0900

    [Bronze V] Title: Hello Judge, Time: 160 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append("Hello World, Judge ").append(i).append("!\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
