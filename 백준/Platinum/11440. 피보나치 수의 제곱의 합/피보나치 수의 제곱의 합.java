824b974f7be034d9b715558242fddaf31ade8422
김기범 <63234082+ejsjdj@users.noreply.github.com>
2026-01-20 00:49:23 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static long[][] matrix = {{1, 1}, {1, 0}};
+    static long mod = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long V = Long.parseLong(br.readLine());
+
+        long[][] result = matrixPower(matrix, V);
+
+        System.out.println(result[0][0] * result[0][1] % mod);
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static long[][] matrixPower(long[][] matrix, long T) {
+        if (T == 1) return matrix;
+
+        if (T % 2 == 0) {
+            long[][] half = matrixPower(matrix, T / 2);
+            return matrixMultiply(half, half);
+        } else {
+            return matrixMultiply(matrix, matrixPower(matrix, T - 1));
+        }
+    }
+
+    static long[][] matrixMultiply(long[][] A, long[][] B) {
+        long[][] result = new long[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+                result[i][j] = result[i][j] % mod;
+            }
+        }
+        return result;
+    }
+}
