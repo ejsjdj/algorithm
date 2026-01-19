a733c5e787a802099cb0acd92b64465d78b62e26
김기범 <63234082+ejsjdj@users.noreply.github.com>
2026-01-19 23:48:39 +0900

    [Platinum V] Title: 피보나치 수와 최대공약수, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Platinum/11778. 피보나치 수와 최대공약수/피보나치 수와 최대공약수.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static long[][] matrix = {{1, 1}, {1, 0}};
+    static long mod = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        long V = gcd(N, M);
+
+        System.out.println(matrixPower(matrix, V)[0][1]);
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static long[][] matrixPower(long[][] matrix, long T) {
+        if (T == 1) return matrix;
+
+        if (T % 2 == 0) {
+            long[][] half = matrixPower(matrix, T / 2);
+            return matrixMultiply(half, half);
+        } else {
+            return matrixMultiply(matrix, matrixPower(matrix, T - 1));
+        }
+    }
+
+    static long[][] matrixMultiply(long[][] A, long[][] B) {
+        long[][] result = new long[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+                result[i][j] = result[i][j] % mod;
+            }
+        }
+        return result;
+    }
+}
