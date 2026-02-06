400b52e4f089b572a4c696ed08715e1f92592f41
김기범 <63234082+ejsjdj@users.noreply.github.com>
2026-02-06 22:36:49 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 472 ms, Memory: 63984 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -33,25 +33,21 @@ public class Main {
             // 스택에 값이 없으면 비운다.
             int current = person[i];
             int len = 1;
-            if (stack.isEmpty()) {
-                stack.push(new Person(len, current));
-            }
-            else {
-                while (!stack.isEmpty()) {
-                    if (stack.peek().height > current) {
-                        cnt += 1;
-                        break;
-                    } else if (stack.peek().height == current) {
-                        cnt += stack.peek().count;
-                        len = stack.peek().count + 1;
-                        stack.pop();
-                    } else {
-                        cnt += stack.peek().count;
-                        stack.pop();
-                    }
+            
+            while (!stack.isEmpty()) {
+                if (stack.peek().height > current) {
+                    cnt += 1;
+                    break;
+                } else if (stack.peek().height == current) {
+                    cnt += stack.peek().count;
+                    len = stack.peek().count + 1;
+                    stack.pop();
+                } else {
+                    cnt += stack.peek().count;
+                    stack.pop();
                 }
-                stack.push(new Person(len, current));
             }
+            stack.push(new Person(len, current));
         }
         System.out.println(cnt);
     }
