4a9078a9a0563376a0dff727caad5fbbdddf55cd
김기범 <ejsjdj@naver.com>
2026-06-23 23:03:56 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,19 +4,10 @@ import java.util.Map.Entry;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        
         HashMap<String, Integer> hm = new HashMap<>();
-        
-        for (String[] cloth : clothes) {
-            hm.put(cloth[1], hm.getOrDefault(cloth[1], 0) + 1);
-        }
-        
-        for (int count : hm.values()) {
-            answer *= (count + 1);
-        }
-        
+        for (String[] cloth : clothes) hm.put(cloth[1], hm.getOrDefault(cloth[1], 0) + 1);
+        for (int count : hm.values()) answer *= (count + 1);
         answer -= 1;
-        
         return answer;
     }
 }
